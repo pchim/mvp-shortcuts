984a1ffb9ceec5556c2395047c9419ba2d414c1f
Peter Chim <pchim24@gmail.com>
2016-05-31 21:57:02 -0700

    Seed database and update npm scripts

--- seed.js
+++ seed.js
@@ -1,4 +1,20 @@
 import fs from 'fs';
+import {db, server} from './dev/server/server.js';
+import shortcutController from './dev/server/shortcut/shortcutController.js';
 
 const shortcuts = JSON.parse(fs.readFileSync('./data.json')).data;
-console.log(shortcuts);
+
+shortcutController.removeAll()
+  .then( data => {
+    shortcutController.insertMany(shortcuts)
+      .then( data => {
+        console.log('Data Seeded Successfully');
+      }).then( () => {
+        db.connection.close();
+        server.close();
+        // shortcutController.getAll()
+        //   .then( data => {
+        //     console.log('Data in database: ', data);
+        //   });
+      });
+  });
