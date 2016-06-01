734b54e197411c2a95ca94139c4c9362e7d64133
Peter Chim <pchim24@gmail.com>
2016-05-31 20:12:12 -0700

    Set up working server and db

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import bodyParser from 'bodyParser';
-import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import db from 'mongoose';
 
 // server and database
 const app = express();
-const db = mongoose.connect('mongodb://localhost:27017/shortcuts');
+db.connect('mongodb://localhost:27017/shortcuts');
 
 // middlewares
 app.use(bodyParser.json());
-require('./routes/routes.js')(app);
+//require('./routes/routes.js')(app);
 
 // listen to port
 const server = app.listen(1337);
